3acdd68fcbc5252dc19602917efc7327224df333
Gavin Chait <whythawk@gmail.com>
2023-08-10 11:03:25 +0200

    Using 'StrictBool' to avoid coercion to boolean

--- whyqd/models/category.py
+++ whyqd/models/category.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 from typing import Union, Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, StrictBool
 from uuid import UUID, uuid4
 
 
 class CategoryModel(BaseModel):
     uuid: UUID = Field(default_factory=uuid4, description="Unique identity for the category. Automatically generated.")
     # Little gotcha ... if bool is 2nd, then any bools are automatically coerced to string ... order is priority.
-    name: Union[bool, str] = Field(..., description="Unique category term.")
+    # Second gotcha, Pydantic is quite liberal with what it classifies as bool https://docs.pydantic.dev/latest/usage/types/booleans/
+    name: Union[StrictBool, str] = Field(..., description="Unique category term.")
     description: Optional[str] = Field(None, description="Description of the unique term")
